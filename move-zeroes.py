25d42dcc9ee73822c3f647d1ec199641539377b8
onestarshang <onestar1967@gmail.com>
2017-04-20 00:00:47 +0800

    two pointers

---
+++ move-zeroes.py
@@ -0,0 +1,32 @@
+# coding=utf8
+
+'''
+Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+For example, given nums = [0, 1, 0, 3, 12], after calling your function, nums should be [1, 3, 12, 0, 0].
+
+Note:
+You must do this in-place without making a copy of the array.
+Minimize the total number of operations.
+'''
+
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        if not nums:
+            return None
+        # left, right = 0, 0
+        # while right < len(nums): # right fast
+        #     if nums[right]:
+        #         nums[left], nums[right] = nums[right], nums[left]
+        #         left += 1
+        #     right +=1
+
+        index = 0
+        for i in range(len(nums)):
+            if nums[i]:
+                nums[i], nums[index] = nums[index], nums[i]
+                index += 1
---
+++ remove-duplicates-from-sorted-array.py
@@ -0,0 +1,33 @@
+# coding=utf8
+
+'''
+Given a sorted array, remove the duplicates in place such that each element
+appear only once and return the new length.
+
+Do not allocate extra space for another array, you must do this in place with
+constant memory.
+
+For example,
+Given input array nums = [1,1,2],
+
+Your function should return length = 2, with the first two elements of nums
+being 1 and 2 respectively. It doesn't matter what you leave beyond the new length.
+'''
+
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        ## two pointers
+
+        if not nums:
+            return 0
+
+        index = 0
+        for i in range(len(nums)):
+            if nums[i] != nums[index]:
+                index += 1
+                nums[index] = nums[i]
+        return index + 1
