a4ff62e2a0d938eeb740046766761b4d2c26bdcf
onestarshang <onestar1967@gmail.com>
2017-04-22 21:54:09 +0800

    search a 2d matrix

--- search-a-2d-matrix-ii.py
+++ search-a-2d-matrix-ii.py
@@ -1,7 +1,8 @@
 # coding=utf8
 
 '''
-Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+Write an efficient algorithm that searches for a value in an m x n matrix.
+This matrix has the following properties:
 
 Integers in each row are sorted in ascending from left to right.
 Integers in each column are sorted in ascending from top to bottom.
