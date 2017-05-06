63f4f76d12a48dee9992e3a80dbe1b7233a55d3d
onestarshang <onestar1967@gmail.com>
2017-05-06 14:50:23 +0800

    bfs

--- binary-tree-level-order-traversal-ii.py
+++ binary-tree-level-order-traversal-ii.py
@@ -1,7 +1,8 @@
 # coding=utf8
 
 '''
-Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right, level by level from leaf to root).
+Given a binary tree, return the bottom-up level order traversal of its nodes'
+values. (ie, from left to right, level by level from leaf to root).
 
 For example:
 Given binary tree [3,9,20,null,null,15,7],
--- binary-tree-level-order-traversal.py
+++ binary-tree-level-order-traversal.py
@@ -1,7 +1,8 @@
 # coding=utf8
 
 '''
-Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+Given a binary tree, return the level order traversal of its nodes' values.
+(ie, from left to right, level by level).
 
 For example:
 Given binary tree [3,9,20,null,null,15,7],
